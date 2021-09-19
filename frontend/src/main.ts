de5e34eb6bda52f914660e8287e9be04ea18d362
Senthuran <senthu1993@gmail.com>
2021-09-19 12:20:07 +0530

    🔥 UI comoponent added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import router from "./router";
+import App from "./App.vue";
+import naive from "./naive";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(naive);
+
+app.mount("#app");
