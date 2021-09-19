33e50d37556c2b1895b20ac645a51bd33963e073
Senthuran <senthu1993@gmail.com>
2021-09-19 22:33:29 +0530

    🌏Api Call added 🌎

---
+++ frontend/src/services/TemperatureServices.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { useAsyncAwait } from "../utils/asyncAwaitHook";
+
+const baseURL = "http://localhost:8081/api/";
+
+const instance = axios.create({
+  baseURL: baseURL,
+});
+
+export const GetAllTemperatureDeatils = async () =>
+  useAsyncAwait(instance.get(`/temperature`));
