0ce9965ab75cd0ebc45d838463a730984999d018
Senthuran <senthu1993@gmail.com>
2021-09-20 10:49:54 +0530

    🏗 interface added

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,10 +1,17 @@
 <template>
   <n-space vertical>
-    <n-select
+    <n-radio-group
       v-model:value="value"
-      :options="options"
-      placeholder="Select Time Intervals"
-    />
+      name="left-size"
+      style="margin-bottom: 12px"
+    >
+      <n-radio-button
+        :value="singleOption.value"
+        v-for="(singleOption, index) in options"
+        :key="index"
+        >{{ singleOption.label }}</n-radio-button
+      >
+    </n-radio-group>
   </n-space>
   <Chart :chartData="chartData" :key="reRenderKey" />
 </template>
@@ -15,27 +22,39 @@ import { defineComponent, reactive, toRefs, onMounted, watch } from "vue";
 import { GetChartByTimeIntervel } from "../services/TemperatureServices";
 import Chart from "../components/Chart/Chart.vue";
 
+interface IOptions {
+  label: string;
+  value: number;
+}
+
+interface IChart {
+  options: Array<IOptions>;
+  value: string;
+  chartData: any;
+  reRenderKey: number;
+}
+
 export default defineComponent({
   components: {
     Chart,
   },
   setup() {
-    const data: any = reactive({
+    const data: IChart = reactive({
       options: [],
-      value: null,
+      value: "1",
       chartData: {},
       reRenderKey: Number(new Date()),
     });
 
     onMounted(async () => {
-      data.options = Array.from(Array(11).keys()).map((x) => {
+      data.options = Array.from(Array(12).keys()).map((x) => {
         return {
           label: `${x + 1} Hours`,
           value: x + 1,
         };
       });
 
-      const [chartData, chartErr] = await GetChartByTimeIntervel(1);
+      const [chartData, chartErr] = await GetChartByTimeIntervel("1");
       data.chartData.value = chartData.data.data.data;
       data.chartData.labels = chartData.data.data.labels;
       data.reRenderKey = Number(new Date());
--- frontend/src/views/Temperature.vue
+++ frontend/src/views/Temperature.vue
@@ -1,32 +1,73 @@
 <template>
+  <n-form inline :label-width="180" size="large" style="padding: 10px">
+    <n-form-item
+      label="Name"
+      :validation-status="formValue.errorMessage ? 'error' : 'success'"
+      :feedback="formValue.errorMessage"
+    >
+      <n-input
+        v-model:value="formValue.hexMessage"
+        placeholder="Enter hex Message"
+        @keypress="
+          () => {
+            formValue.errorMessage = '';
+          }
+        "
+      />
+    </n-form-item>
+    <n-form-item style="padding-left: 10px">
+      <n-button @click="OnClick">Validate</n-button>
+    </n-form-item>
+  </n-form>
+
   <n-data-table :columns="columns" :data="data" :pagination="pagination" />
 </template>
 <script lang="ts">
-import { defineComponent, reactive, toRefs, onMounted } from "vue";
+import { defineComponent, reactive, toRefs, onMounted, ref } from "vue";
+import { useMessage } from "naive-ui";
 
-import { GetAllTemperatureDeatils } from "../services/TemperatureServices";
+import {
+  GetAllTemperatureDeatils,
+  AddHexMessage,
+} from "../services/TemperatureServices";
+import { isHexaNumber } from "../utils/isHexaNumber";
+
+interface IColumn {
+  title: string;
+  key: string;
+  sorter?: (rowA: any, rowB: any) => number;
+}
+
+interface IDataTable {
+  columns: IColumn[];
+  data: any;
+  pagination: {
+    IColumn: number;
+  };
+}
 
 export default defineComponent({
   setup() {
-    const data: any = reactive({
+    const formValue = ref({
+      hexMessage: "",
+      errorMessage: "",
+    });
+
+    const data: IDataTable = reactive({
       columns: [],
       data: [],
       pagination: {
-        pageSize: 10,
+        IColumn: 10,
       },
     });
 
+    const message = useMessage();
     onMounted(() => {
       GetAllTemperatureCol();
     });
 
     const GetAllTemperatureCol = async () => {
-      const [
-        {
-          data: { data: listData },
-        },
-        listErr,
-      ] = await GetAllTemperatureDeatils();
+      const [listData, listErr] = await GetAllTemperatureDeatils();
 
       if (listErr) {
         return listErr;
@@ -52,10 +93,39 @@ export default defineComponent({
           key: "bigEndianMessage",
         },
       ];
-      data.data = listData;
+      data.data = listData.data.data;
+    };
+
+    const OnClick = async (e: any) => {
+      e.preventDefault();
+      if (!formValue.value.hexMessage) {
+        return (formValue.value.errorMessage = "Please enter the value");
+      }
+
+      if (
+        !isHexaNumber(formValue.value.hexMessage) ||
+        formValue.value.hexMessage.length != 10
+      ) {
+        return (formValue.value.errorMessage = "Invalid Hex Message");
+      }
+
+      const [, hexError] = await AddHexMessage(formValue.value.hexMessage);
+
+      if (hexError) {
+        message.error("Invalid hex");
+      } else {
+        formValue.value.hexMessage = "";
+        formValue.value.errorMessage = "";
+        GetAllTemperatureCol();
+        message.success("Hey Added successfully");
+      }
+
+      return null;
     };
 
     return {
+      formValue,
+      OnClick,
       ...toRefs(data),
     };
   },
