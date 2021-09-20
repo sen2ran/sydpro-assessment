ebb8b29ba93c2b2f316296cb862db234faaa3f68
Senthuran <senthu1993@gmail.com>
2021-09-20 10:49:23 +0530

    🌐 AddHexMessage Service added

--- frontend/src/services/TemperatureServices.ts
+++ frontend/src/services/TemperatureServices.ts
@@ -10,5 +10,8 @@ const instance = axios.create({
 export const GetAllTemperatureDeatils = async () =>
   useAsyncAwait(instance.get(`/temperature`));
 
-export const GetChartByTimeIntervel = async (timeIntervel: number) =>
+export const GetChartByTimeIntervel = async (timeIntervel: string) =>
   useAsyncAwait(instance.get(`/temperature/chart/${timeIntervel}`));
+
+export const AddHexMessage = async (hexPayload: string) =>
+  useAsyncAwait(instance.post(`/temperature`, { hexPayload: hexPayload }));
