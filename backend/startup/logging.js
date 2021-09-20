c61210ba2fb6674eac1bfa183f9c1890663a8c41
Senthuran <senthu1993@gmail.com>
2021-09-20 11:07:33 +0530

    📃logging unhandle error and connection

---
+++ backend/startup/logging.js
@@ -0,0 +1,18 @@
+const winston = require("winston");
+require("express-async-errors");
+
+module.exports = function () {
+  winston.exceptions.handle(
+    new winston.transports.File({
+      filename: "errorlog/uncaughtExceptions.json",
+    })
+  );
+  process.on("unhandledRejection", (ex) => {
+    throw ex;
+  });
+  winston.add(
+    new winston.transports.File({
+      filename: "errorlog/logfile.log",
+    })
+  );
+};
