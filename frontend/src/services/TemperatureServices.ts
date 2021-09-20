bf474ff0a9ad7537aed68272e40a2c0e52985857
Senthuran <senthu1993@gmail.com>
2021-09-20 12:01:18 +0530

    🌐baseurl changed

--- frontend/src/services/TemperatureServices.ts
+++ frontend/src/services/TemperatureServices.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAsyncAwait } from "../utils/asyncAwaitHook";
 
-const baseURL = "http://localhost:8081/api/";
+const baseURL = "http://localhost:3001/api/";
 
 const instance = axios.create({
   baseURL: baseURL,
