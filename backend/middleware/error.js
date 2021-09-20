9e735a251b52b00d8a5b175fb3391333e2a13fae
Senthuran <senthu1993@gmail.com>
2021-09-20 11:06:22 +0530

    Middleware added for unhanddle error

---
+++ backend/middleware/error.js
@@ -0,0 +1,12 @@
+const winston = require("winston");
+
+module.exports = function (err, req, res, next) {
+  winston.error(err.message, err);
+  console.log(err);
+  res.status(500).send({
+    success: false,
+    status: "Internal Server Error",
+    details: "Server error. Please try again in a bit.",
+  });
+};
+// Every Backend Error Message Go By this
